218085731975836fb655966b8ad26625089065d4
Mostafa <mostafa.saneisn@gmail.com>
2024-01-05 12:49:30 +0330

    update: creating peyk models

--- peyk/models.py
+++ peyk/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Province(models.Model):
+    name = models.CharField(max_length=50)
+
+    @property
+    def city(self):
+        return self.city()
+
+
+class City(models.Model):
+    name = models.CharField(max_length=50)
+    province = models.ForeignKey(Province, on_delete=models.PROTECT)
+
+
+class Origin(models.Model):
+    city = models.ForeignKey(City, on_delete=models.PROTECT)
+    address = models.TextField()
+    name = models.CharField( max_length=50)
+    phone_number = models.CharField(max_length=50)
+    No =models.CharField( max_length=4)
+    Unit = models.CharField( max_length=3)                  
+    description = models.TextField()
+
+
+class Destination(models.Model):
+    city = models.ForeignKey(City, on_delete=models.PROTECT)
+    address = models.TextField()
+    name = models.CharField( max_length=50)
+    phone_number = models.CharField(max_length=50)
+    No =models.CharField( max_length=4)
+    Unit = models.CharField( max_length=3)
+    description = models.TextField()
+
+
+class Order(models.Model):
+    origin = models.ForeignKey(Origin, on_delete=models.PROTECT)
+    destination = models.ForeignKey(Destination, on_delete=models.PROTECT)
